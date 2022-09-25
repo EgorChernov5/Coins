a632a5d3ca5b00b1d15ca812fd21dfdc107adf3b
EgorChernov3140 <egorchernov3140@gmail.com>
2022-09-25 18:27:13 +0300

    Add Chips

--- app/src/main/java/com/example/coins/MainActivity.kt
+++ app/src/main/java/com/example/coins/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.coins
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 class MainActivity : AppCompatActivity() {
+    var currency: CharSequence? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val chipsGroup = findViewById<ChipGroup>(R.id.chips_group)
+        val chip = chipsGroup.findViewById<Chip>(R.id.usd)
+        chip.isChecked = true
+        currency = chip.text
+
+        chipsGroup.setOnCheckedChangeListener { chipGroup: ChipGroup, checkedId: Int ->
+            currency = chipGroup.findViewById<Chip>(checkedId)?.text
+            Toast.makeText(chipGroup.context, currency ?: "No Choice", Toast.LENGTH_SHORT).show()
+        }
     }
 }
